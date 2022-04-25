8405d9efb3510d5e48228c684492c912e2c206a3
Ilias Trichopoulos <hlias.nop@gmail.com>
2022-04-25 14:15:22 +0200

    Init

---
+++ main-cjs.js
@@ -0,0 +1,4 @@
+const sdk = require("@alephium/sdk");
+
+console.log("hello, CJS world");
+console.log(sdk);
---
+++ main-esm.js
@@ -0,0 +1,4 @@
+import * as sdk from "@alephium/sdk";
+
+console.log("hello, ESM world");
+console.log(sdk);
